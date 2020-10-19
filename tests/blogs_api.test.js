815ae2e381ddb49a03c5d619fcabf4dc82867c9f
Zekeriya <zekeriyaerturk@gmail.com>
2020-10-19 12:36:03 +0300

    exercise 4.10

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -20,6 +20,30 @@ test("check blogs id's are not named _id", async () => {
   });
 });
 
+test("check adding blog is working", async () => {
+  const newBlog = {
+    title: "A blog about JS",
+    author: "John Doe",
+    url: "www.somesite.com",
+    likes: 10,
+  };
+
+  const res1 = await api.get("/api/blogs");
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const res2 = await api.get("/api/blogs");
+
+  const titles = res2.body.map((r) => r.title);
+
+  expect(res2.body).toHaveLength(res1.body.length + 1);
+  expect(titles).toContain("A blog about JS");
+});
+
 afterAll(() => {
   moongose.connection.close();
 });
