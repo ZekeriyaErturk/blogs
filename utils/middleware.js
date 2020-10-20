75fecb7f3c7ff3be8b250e0752618f273bdfd87d
Zekeriya <zekeriyaerturk@gmail.com>
2020-10-20 13:10:12 +0300

    exercise 4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -22,11 +22,21 @@ const errorHandler = (err, req, res, next) => {
   next(err);
 };
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    req.token = authorization.substring(7);
+  }
+
+  next();
+};
+
 const unknownEndPoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
 module.exports = {
+  tokenExtractor,
   requestLogger,
   unknownEndPoint,
   errorHandler,
